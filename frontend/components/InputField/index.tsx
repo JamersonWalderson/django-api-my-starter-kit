6deb9c85cc444cfe03c30ebaeb0f91a7889a08b7
Jamerson Walderson <jamersonwalderson@gmail.com>
2023-02-05 22:04:30 -0300

    feat: componente input field

---
+++ frontend/components/InputField/index.tsx
@@ -0,0 +1,20 @@
+import { FormControl, FormHelperText, FormLabel, Input } from "@chakra-ui/react"
+
+const InputField = ({ name, label, ...props }) => {
+    return (
+      <FormControl isInvalid={props.formik.errors[name] && props.formik.touched[name]}>
+        <FormLabel htmlFor={name}>{label}</FormLabel>
+        <Input
+          id={name}
+          name={name}
+          onChange={props.formik.handleChange}
+          onBlur={props.formik.handleBlur}
+          value={props.formik.values[name]}
+          {...props}
+        />
+        {props.formik.errors[name] && props.formik.touched[name] ? (
+          <FormHelperText id={`${name}-error`}>{props.formik.errors[name]}</FormHelperText>
+        ) : null}
+      </FormControl>
+    )
+  }
