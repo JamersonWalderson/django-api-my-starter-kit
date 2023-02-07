609f8a194c23246ba3edf99b2c6976f784c139a9
Jamerson Walderson <jamersonwalderson@gmail.com>
2023-02-07 10:25:36 -0300

    chore: exemplificação de permisison na view

--- backend/src/apps/service/api/v1/views.py
+++ backend/src/apps/service/api/v1/views.py
@@ -1,8 +1,10 @@
 from src.apps.service.api.v1.serializers import Service, ServiceSerializer
 from src.apps.service.models import Service
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 class ServiceViewSet(viewsets.ModelViewSet):
     serializer_class = ServiceSerializer
     queryset = Service.objects.all()
+    # permission_classes = (IsAuthenticated,)
